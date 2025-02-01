50906189b0cea38cc77f9a9423c0486aa033b39e
banimania <dani17val+1@gmail.com>
2025-02-01 04:12:34 +0100

    math and dfs

---
+++ Codeforces/1948C.cpp
@@ -0,0 +1,202 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vint;
+
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define REPV(i, a, b) for (int i = a; i >= b; i--)
+#define SCAN(v) for (int i = 0; i < v.size(); i++) cin >> v[i]
+#define REPLIST(x, v) for (auto x : v)
+#define PRINT(v) for (auto x : v) { cout << x << " "; } cout << endl;
+#define SORTINC(v) sort(v.begin(), v.end(), [](auto &x, auto &y) { return x < y; });
+#define SORTDEC(v) sort(v.begin(), v.end(), [](auto &x, auto &y) { return x > y; });
+#define MIN(v) min_element(v.begin(), v.end()) - v.begin()
+#define MAX(v) max_element(v.begin(), v.end()) - v.begin()
+
+const ll MOD = 1e9 + 7;
+
+ll modExp(ll base, ll exp, ll mod) {
+  ll result = 1;
+  base %= mod;
+
+  while (exp > 0) {
+    if (exp % 2 == 1) result = (result * base) % mod;
+
+    base = (base * base) % mod;
+    exp /= 2;
+  }
+
+  return result;
+}
+
+void make_set(vector<int> &nodoPadre, int v) {
+  nodoPadre[v] = v;
+}
+
+int find_set(vector<int> &nodoPadre, int v) {
+  if (v == nodoPadre[v]) return v;
+  return find_set(nodoPadre, nodoPadre[v]);
+}
+
+void union_sets(vector<int> &nodoPadre, int a, int b) {
+  a = find_set(nodoPadre, a);
+  b = find_set(nodoPadre, b);
+  if (a != b) nodoPadre[b] = a;
+}
+
+template <typename T, typename Op>
+class SegTree {
+public:
+  vector<T> tree;
+  int n;
+  Op op;
+
+  SegTree(const vector<T> & v) {
+    n = v.size();
+    tree.resize(4 * n);
+
+    build(v, 0, 0, v.size() - 1);
+  }
+
+  void build(const vector<T>& arr, int node, int start, int end) {
+    if (start == end) {
+      tree[node] = arr[start];
+    } else {
+      int mid = (start + end) / 2;
+      int leftChild = 2 * node + 1;
+      int rightChild = 2 * node + 2;
+      
+      build(arr, leftChild, start, mid);
+      build(arr, rightChild, mid + 1, end);
+
+      tree[node] = op(tree[leftChild], tree[rightChild]);
+    }
+  }
+
+  T query(int l, int r) {
+    return query(0, 0, n - 1, l, r);
+  }
+
+  T query(int node, int start, int end, int L, int R) {
+    if (R < start || end < L) {
+      return op.id();
+    }
+    if (L <= start && end <= R) {
+      return tree[node];
+    }
+
+    int mid = (start + end) / 2;
+    int leftChild = 2 * node + 1;
+    int rightChild = 2 * node + 2;
+
+    T leftResult = query(leftChild, start, mid, L, R);
+    T rightResult = query(rightChild, mid + 1, end, L, R);
+
+    return op(leftResult, rightResult);
+  }
+
+  void update(int idx, T val) {
+    update(0, 0, n - 1, idx, val);
+  }
+
+  void update(int node, int start, int end, int idx, T val) {
+    if (start == end) {
+      tree[node] = val;
+    } else {
+      int mid = (start + end) / 2;
+      int leftChild = 2 * node + 1;
+      int rightChild = 2 * node + 2;
+
+      if (start <= idx && idx <= mid) {
+        update(leftChild, start, mid, idx, val);
+      } else {
+        update(rightChild, mid + 1, end, idx, val);
+      }
+
+      tree[node] = op(tree[leftChild], tree[rightChild]);
+    }
+  }
+};
+
+struct SumOp {
+  using T = ll;
+
+  T operator()(T a, T b) const {
+    return a + b;
+  }
+
+  T id() const {
+    return 0;
+  }
+};
+
+struct MinOp {
+  using T = ll;
+
+  T operator()(T a, T b) const {
+    return min(a, b);
+  }
+
+  T id() const {
+    return LLONG_MAX;
+  }
+};
+
+#define TESTCASES 1
+
+set<pair<int, int>> dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+void solve() {
+  ll n;
+  cin >> n;
+  vector<vector<char>> a(2, vector<char>(n));
+  REP (i, 0, 2) {
+    REP (j, 0, n) {
+      cin >> a[i][j];
+    }
+  }
+
+  stack<pair<ll, ll>> st;
+  vector<vector<bool>> visited(2, vector<bool>(n, false));
+  visited[0][0] = true;
+  st.push({0, 0});
+  while (!st.empty()) {
+    ll i = st.top().first;
+    ll j = st.top().second;
+    st.pop();
+
+    if (i == 1 && j == n - 1) {
+      cout << "YES" << endl;
+      return;
+    }
+
+
+    for (auto [dirI, dirJ] : dirs) {
+      ll newI = i + dirI;
+      ll newJ = j + dirJ;
+
+      if (newI >= 0 && newI < 2 && newJ >= 0 && newJ < n) {
+        if (a[newI][newJ] == '<') newJ--;
+        else newJ++;
+        if (!visited[newI][newJ]) {
+          visited[newI][newJ] = true;
+          st.push({newI, newJ});
+        }
+      }
+    }
+
+  }
+
+  cout << "NO" << endl;
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int t = 1;
+  if (TESTCASES) cin >> t;
+  while (t--) solve();
+  return 0;
+}
