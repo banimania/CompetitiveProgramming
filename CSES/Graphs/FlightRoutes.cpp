8eb8da3d496cb5b197fd4a45294e4760c219bfe6
banimania <dani17val+1@gmail.com>
2025-02-02 20:48:56 +0100

    K shortest paths

---
+++ CSES/Graphs/FlightRoutes.cpp
@@ -0,0 +1,214 @@
+// https://cses.fi/problemset/task/1196
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vint;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define REPV(i, a, b) for (int i = a; i >= b; i--)
+#define SCAN(v) for (int i = 0; i < v.size(); i++) cin >> v[i]
+#define REPLIST(x, v) for (auto x : v)
+#define PRINT(v) for (auto x : v) { cout << x << " "; } cout << endl;
+#define SORTINC(v) sort(v.begin(), v.end(), [](auto &x, auto &y) { return x < y; });
+#define SORTDEC(v) sort(v.begin(), v.end(), [](auto &x, auto &y) { return x > y; });
+#define MIN(v) min_element(v.begin(), v.end()) - v.begin()
+#define MAX(v) max_element(v.begin(), v.end()) - v.begin()
+
+const ll MOD = 1e9 + 7;
+
+ll modExp(ll base, ll exp, ll mod) {
+  ll result = 1;
+  base %= mod;
+
+  while (exp > 0) {
+    if (exp % 2 == 1) result = (result * base) % mod;
+
+    base = (base * base) % mod;
+    exp /= 2;
+  }
+
+  return result;
+}
+
+void make_set(vector<int> &nodoPadre, int v) {
+  nodoPadre[v] = v;
+}
+
+int find_set(vector<int> &nodoPadre, int v) {
+  if (v == nodoPadre[v]) return v;
+  return find_set(nodoPadre, nodoPadre[v]);
+}
+
+void union_sets(vector<int> &nodoPadre, int a, int b) {
+  a = find_set(nodoPadre, a);
+  b = find_set(nodoPadre, b);
+  if (a != b) nodoPadre[b] = a;
+}
+
+template <typename T, typename Op>
+class SegTree {
+public:
+  vector<T> tree;
+  int n;
+  Op op;
+
+  SegTree(const vector<T> & v) {
+    n = v.size();
+    tree.resize(4 * n);
+
+    build(v, 0, 0, v.size() - 1);
+  }
+
+  void build(const vector<T>& arr, int node, int start, int end) {
+    if (start == end) {
+      tree[node] = arr[start];
+    } else {
+      int mid = (start + end) / 2;
+      int leftChild = 2 * node + 1;
+      int rightChild = 2 * node + 2;
+
+      build(arr, leftChild, start, mid);
+      build(arr, rightChild, mid + 1, end);
+
+      tree[node] = op(tree[leftChild], tree[rightChild]);
+    }
+  }
+
+  T query(int l, int r) {
+    return query(0, 0, n - 1, l, r);
+  }
+
+  T query(int node, int start, int end, int L, int R) {
+    if (R < start || end < L) {
+      return op.id();
+    }
+    if (L <= start && end <= R) {
+      return tree[node];
+    }
+
+    int mid = (start + end) / 2;
+    int leftChild = 2 * node + 1;
+    int rightChild = 2 * node + 2;
+
+    T leftResult = query(leftChild, start, mid, L, R);
+    T rightResult = query(rightChild, mid + 1, end, L, R);
+
+    return op(leftResult, rightResult);
+  }
+
+  void update(int idx, T val) {
+    update(0, 0, n - 1, idx, val);
+  }
+
+  void update(int node, int start, int end, int idx, T val) {
+    if (start == end) {
+      tree[node] = val;
+    } else {
+      int mid = (start + end) / 2;
+      int leftChild = 2 * node + 1;
+      int rightChild = 2 * node + 2;
+
+      if (start <= idx && idx <= mid) {
+        update(leftChild, start, mid, idx, val);
+      } else {
+        update(rightChild, mid + 1, end, idx, val);
+      }
+
+      tree[node] = op(tree[leftChild], tree[rightChild]);
+    }
+  }
+};
+
+struct SumOp {
+  using T = ll;
+
+  T operator()(T a, T b) const {
+    return a + b;
+  }
+
+  T id() const {
+    return 0;
+  }
+};
+
+struct MinOp {
+  using T = ll;
+
+  T operator()(T a, T b) const {
+    return min(a, b);
+  }
+
+  T id() const {
+    return LLONG_MAX;
+  }
+};
+
+#define TESTCASES 0
+
+struct State {
+  ll city, cost;
+};
+
+void solve() {
+  ll n, m, k;
+  cin >> n >> m >> k;
+
+  vector<vector<pair<ll, ll>>> adj(n);
+  for (int i = 0; i < m; i++) {
+    ll u, v, l;
+    cin >> u >> v >> l;
+    u--, v--;
+
+    adj[u].push_back({v, l});
+  }
+
+
+  vector<priority_queue<ll>> bestDist(n);
+  priority_queue<State, vector<State>, function<bool(State, State)>> pq([](const State &a, const State &b) {
+    return a.cost > b.cost;
+  });
+
+  bestDist[0].push(0);
+  pq.push({0, 0});
+
+  while (!pq.empty()) {
+    State current = pq.top();
+		pq.pop();
+		if (current.cost > bestDist[current.city].top()) continue;
+		for (auto [neighbour, cost] : adj[current.city]) {
+			ll newCost = current.cost + cost;
+			if (bestDist[neighbour].size() < k) {
+				bestDist[neighbour].push(newCost);
+				pq.push({neighbour, newCost});
+			} else if (newCost < bestDist[neighbour].top()) {
+        bestDist[neighbour].pop();
+        bestDist[neighbour].push(newCost);
+				pq.push({neighbour, newCost});
+			}
+		}
+	}
+
+
+  vector<ll> ans;
+  while (!bestDist[n - 1].empty()) {
+    ans.push_back(bestDist[n - 1].top());
+    bestDist[n - 1].pop();
+  }
+  reverse(ans.begin(), ans.end());
+  PRINT(ans);
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int t = 1;
+  if (TESTCASES) cin >> t;
+  while (t--) solve();
+  return 0;
+}
