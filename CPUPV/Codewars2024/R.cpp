482d888749056592c340455f0efd7eb91980692c
banimania <dani17val+1@gmail.com>
2025-02-08 14:42:00 +0100

    codewars

---
+++ CPUPV/Codewars2024/R.cpp
@@ -0,0 +1,200 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vint;
+
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define REPV(i, a, b) for (int i = a; i >= b; i--)
+#define SCAN(v) for (int i = 0; i < v.size(); i++) cin >> v[i]
+#define REPLIST(x, v) for (auto x : v)
+#define PRINT(v) for (auto x : v) { cout << x << " "; } cout << endl;
+#define SORTINC(v) sort(v.begin(), v.end(), [](auto &x, auto &y) { return x < y; });
+#define SORTDEC(v) sort(v.begin(), v.end(), [](auto &x, auto &y) { return x > y; });
+#define MIN(v) min_element(v.begin(), v.end()) - v.begin()
+#define MAX(v) max_element(v.begin(), v.end()) - v.begin()
+
+const ll MOD = 1e9 + 7;
+
+ll modExp(ll base, ll exp, ll mod) {
+  ll result = 1;
+  base %= mod;
+
+  while (exp > 0) {
+    if (exp % 2 == 1) result = (result * base) % mod;
+
+    base = (base * base) % mod;
+    exp /= 2;
+  }
+
+  return result;
+}
+
+void make_set(vector<int> &nodoPadre, int v) {
+  nodoPadre[v] = v;
+}
+
+int find_set(vector<int> &nodoPadre, int v) {
+  if (v == nodoPadre[v]) return v;
+  return find_set(nodoPadre, nodoPadre[v]);
+}
+
+void union_sets(vector<int> &nodoPadre, int a, int b) {
+  a = find_set(nodoPadre, a);
+  b = find_set(nodoPadre, b);
+  if (a != b) nodoPadre[b] = a;
+}
+
+template <typename T, typename Op>
+class SegTree {
+public:
+  vector<T> tree;
+  int n;
+  Op op;
+
+  SegTree(const vector<T> & v) {
+    n = v.size();
+    tree.resize(4 * n);
+
+    build(v, 0, 0, v.size() - 1);
+  }
+
+  void build(const vector<T>& arr, int node, int start, int end) {
+    if (start == end) {
+      tree[node] = arr[start];
+    } else {
+      int mid = (start + end) / 2;
+      int leftChild = 2 * node + 1;
+      int rightChild = 2 * node + 2;
+      
+      build(arr, leftChild, start, mid);
+      build(arr, rightChild, mid + 1, end);
+
+      tree[node] = op(tree[leftChild], tree[rightChild]);
+    }
+  }
+
+  T query(int l, int r) {
+    return query(0, 0, n - 1, l, r);
+  }
+
+  T query(int node, int start, int end, int L, int R) {
+    if (R < start || end < L) {
+      return op.id();
+    }
+    if (L <= start && end <= R) {
+      return tree[node];
+    }
+
+    int mid = (start + end) / 2;
+    int leftChild = 2 * node + 1;
+    int rightChild = 2 * node + 2;
+
+    T leftResult = query(leftChild, start, mid, L, R);
+    T rightResult = query(rightChild, mid + 1, end, L, R);
+
+    return op(leftResult, rightResult);
+  }
+
+  void update(int idx, T val) {
+    update(0, 0, n - 1, idx, val);
+  }
+
+  void update(int node, int start, int end, int idx, T val) {
+    if (start == end) {
+      tree[node] = val;
+    } else {
+      int mid = (start + end) / 2;
+      int leftChild = 2 * node + 1;
+      int rightChild = 2 * node + 2;
+
+      if (start <= idx && idx <= mid) {
+        update(leftChild, start, mid, idx, val);
+      } else {
+        update(rightChild, mid + 1, end, idx, val);
+      }
+
+      tree[node] = op(tree[leftChild], tree[rightChild]);
+    }
+  }
+};
+
+struct SumOp {
+  using T = ll;
+
+  T operator()(T a, T b) const {
+    return a + b;
+  }
+
+  T id() const {
+    return 0;
+  }
+};
+
+struct MinOp {
+  using T = ll;
+
+  T operator()(T a, T b) const {
+    return min(a, b);
+  }
+
+  T id() const {
+    return LLONG_MAX;
+  }
+};
+
+#define TESTCASES 0
+
+ll maxLen(ll i, ll k, vll &a, vll &prefixSum, vll &dp) {
+  if (i < 0) return 0;
+  if (dp[i] != -1) return dp[i];
+
+  dp[i] = 0;
+
+  // nuevo
+  if (a[i] <= k) {
+    dp[i] = 1;
+  }
+
+  // unirse
+  if (i - 1 >= 0) {
+    ll newLen = maxLen(i - 1, k, a, prefixSum, dp) + 1;
+    ll newSum = prefixSum[i] - (i - newLen >= 0 ? prefixSum[i - newLen] : 0);    
+    if (newSum <= k) {
+      dp[i] = max(dp[i], newLen);
+    }
+  }
+  return dp[i];
+}
+
+void solve() {
+  ll n, k;
+  cin >> n >> k;
+  vll a(n);
+  SCAN(a);
+  vll prefixSum(n);
+  prefixSum[0] = a[0];
+  REP (i, 1, n) {
+    prefixSum[i] = prefixSum[i - 1] + a[i];
+  }
+
+  vll dp(n, -1);
+
+  ll ans = 0;
+  REP (i, 0, n) {
+    ans = max(ans, maxLen(i, k, a, prefixSum, dp));
+  }
+
+  cout << ans << endl;
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int t = 1;
+  if (TESTCASES) cin >> t;
+  while (t--) solve();
+  return 0;
+}
