6672e25b834888e5c7a5c2c2e1b75f28587796b4
banimania <dani17val+1@gmail.com>
2025-02-14 08:13:20 +0100

    codeforces

---
+++ Codeforces/1980B.cpp
@@ -0,0 +1,206 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vint;
+
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define REPV(i, a, b) for (int i = a; i >= b; i--)
+#define SCAN(v) for (int i = 0; i < v.size(); i++) cin >> v[i]
+#define REPLIST(x, v) for (auto x : v)
+#define PRINT(v) for (auto x : v) { cout << x << " "; } cout << endl;
+#define SORTINC(v) sort(v.begin(), v.end(), [](auto &x, auto &y) { return x < y; });
+#define SORTDEC(v) sort(v.begin(), v.end(), [](auto &x, auto &y) { return x > y; });
+#define MIN(v) min_element(v.begin(), v.end()) - v.begin()
+#define MAX(v) max_element(v.begin(), v.end()) - v.begin()
+
+const ll MOD = 1e9 + 7;
+
+ll fastPow(ll a, ll b) {
+  ll res = 1;
+  while (b > 0) {
+    if (b & 1) res = res * a;
+    a = a * a;
+    b >>= 1;
+  }
+  return res;
+}
+
+ll modExp(ll base, ll exp, ll mod) {
+  ll result = 1;
+  base %= mod;
+
+  while (exp > 0) {
+    if (exp % 2 == 1) result = (result * base) % mod;
+
+    base = (base * base) % mod;
+    exp /= 2;
+  }
+
+  return result;
+}
+
+void make_set(vector<int> &nodoPadre, int v) {
+  nodoPadre[v] = v;
+}
+
+int find_set(vector<int> &nodoPadre, int v) {
+  if (v == nodoPadre[v]) return v;
+  return find_set(nodoPadre, nodoPadre[v]);
+}
+
+void union_sets(vector<int> &nodoPadre, int a, int b) {
+  a = find_set(nodoPadre, a);
+  b = find_set(nodoPadre, b);
+  if (a != b) nodoPadre[b] = a;
+}
+
+template <typename T, typename Op>
+class SegTree {
+public:
+  vector<T> tree;
+  int n;
+  Op op;
+
+  SegTree(const vector<T> & v) {
+    n = v.size();
+    tree.resize(4 * n);
+
+    build(v, 0, 0, v.size() - 1);
+  }
+
+  void build(const vector<T>& arr, int node, int start, int end) {
+    if (start == end) {
+      tree[node] = arr[start];
+    } else {
+      int mid = (start + end) / 2;
+      int leftChild = 2 * node + 1;
+      int rightChild = 2 * node + 2;
+      
+      build(arr, leftChild, start, mid);
+      build(arr, rightChild, mid + 1, end);
+
+      tree[node] = op(tree[leftChild], tree[rightChild]);
+    }
+  }
+
+  T query(int l, int r) {
+    return query(0, 0, n - 1, l, r);
+  }
+
+  T query(int node, int start, int end, int L, int R) {
+    if (R < start || end < L) {
+      return op.id();
+    }
+    if (L <= start && end <= R) {
+      return tree[node];
+    }
+
+    int mid = (start + end) / 2;
+    int leftChild = 2 * node + 1;
+    int rightChild = 2 * node + 2;
+
+    T leftResult = query(leftChild, start, mid, L, R);
+    T rightResult = query(rightChild, mid + 1, end, L, R);
+
+    return op(leftResult, rightResult);
+  }
+
+  void update(int idx, T val) {
+    update(0, 0, n - 1, idx, val);
+  }
+
+  void update(int node, int start, int end, int idx, T val) {
+    if (start == end) {
+      tree[node] = val;
+    } else {
+      int mid = (start + end) / 2;
+      int leftChild = 2 * node + 1;
+      int rightChild = 2 * node + 2;
+
+      if (start <= idx && idx <= mid) {
+        update(leftChild, start, mid, idx, val);
+      } else {
+        update(rightChild, mid + 1, end, idx, val);
+      }
+
+      tree[node] = op(tree[leftChild], tree[rightChild]);
+    }
+  }
+};
+
+struct SumOp {
+  using T = ll;
+
+  T operator()(T a, T b) const {
+    return a + b;
+  }
+
+  T id() const {
+    return 0;
+  }
+};
+
+struct MinOp {
+  using T = ll;
+
+  T operator()(T a, T b) const {
+    return min(a, b);
+  }
+
+  T id() const {
+    return LLONG_MAX;
+  }
+};
+
+#define TESTCASES 1
+
+void solve() {
+  ll n, f, k;
+  cin >> n >> f >> k;
+
+  ll favouriteNum = -1;
+  f--;
+  vector<ll> cubes(n);
+  REP (i, 0, cubes.size()) {
+    cin >> cubes[i];
+
+    if (i == f) {
+      favouriteNum = cubes[i];
+    }
+  }
+
+  sort(cubes.begin(), cubes.end(), [](const ll &a, const ll &b) {
+    return a > b;
+  });
+
+  int state = 0; // NO
+  REP (i, 0, k) {
+    if (cubes[i] == favouriteNum) {
+      state = 2; //YES;
+    }
+  }
+
+  if (state == 2 && k < n) {
+    if (cubes[k] == favouriteNum) state = 1;
+  }
+
+  if (state == 0) {
+    cout << "NO" << endl;
+  } else if (state == 1) {
+    cout << "MAYBE" << endl;
+  } else {
+    cout << "YES" << endl;
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int t = 1;
+  if (TESTCASES) cin >> t;
+  while (t--) solve();
+  return 0;
+}
---
+++ Codeforces/1980C.cpp
@@ -0,0 +1,199 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vint;
+
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define REPV(i, a, b) for (int i = a; i >= b; i--)
+#define SCAN(v) for (int i = 0; i < v.size(); i++) cin >> v[i]
+#define REPLIST(x, v) for (auto x : v)
+#define PRINT(v) for (auto x : v) { cout << x << " "; } cout << endl;
+#define SORTINC(v) sort(v.begin(), v.end(), [](auto &x, auto &y) { return x < y; });
+#define SORTDEC(v) sort(v.begin(), v.end(), [](auto &x, auto &y) { return x > y; });
+#define MIN(v) min_element(v.begin(), v.end()) - v.begin()
+#define MAX(v) max_element(v.begin(), v.end()) - v.begin()
+
+const ll MOD = 1e9 + 7;
+
+ll modExp(ll base, ll exp, ll mod) {
+  ll result = 1;
+  base %= mod;
+
+  while (exp > 0) {
+    if (exp % 2 == 1) result = (result * base) % mod;
+
+    base = (base * base) % mod;
+    exp /= 2;
+  }
+
+  return result;
+}
+
+void make_set(vector<int> &nodoPadre, int v) {
+  nodoPadre[v] = v;
+}
+
+int find_set(vector<int> &nodoPadre, int v) {
+  if (v == nodoPadre[v]) return v;
+  return find_set(nodoPadre, nodoPadre[v]);
+}
+
+void union_sets(vector<int> &nodoPadre, int a, int b) {
+  a = find_set(nodoPadre, a);
+  b = find_set(nodoPadre, b);
+  if (a != b) nodoPadre[b] = a;
+}
+
+template <typename T, typename Op>
+class SegTree {
+public:
+  vector<T> tree;
+  int n;
+  Op op;
+
+  SegTree(const vector<T> & v) {
+    n = v.size();
+    tree.resize(4 * n);
+
+    build(v, 0, 0, v.size() - 1);
+  }
+
+  void build(const vector<T>& arr, int node, int start, int end) {
+    if (start == end) {
+      tree[node] = arr[start];
+    } else {
+      int mid = (start + end) / 2;
+      int leftChild = 2 * node + 1;
+      int rightChild = 2 * node + 2;
+      
+      build(arr, leftChild, start, mid);
+      build(arr, rightChild, mid + 1, end);
+
+      tree[node] = op(tree[leftChild], tree[rightChild]);
+    }
+  }
+
+  T query(int l, int r) {
+    return query(0, 0, n - 1, l, r);
+  }
+
+  T query(int node, int start, int end, int L, int R) {
+    if (R < start || end < L) {
+      return op.id();
+    }
+    if (L <= start && end <= R) {
+      return tree[node];
+    }
+
+    int mid = (start + end) / 2;
+    int leftChild = 2 * node + 1;
+    int rightChild = 2 * node + 2;
+
+    T leftResult = query(leftChild, start, mid, L, R);
+    T rightResult = query(rightChild, mid + 1, end, L, R);
+
+    return op(leftResult, rightResult);
+  }
+
+  void update(int idx, T val) {
+    update(0, 0, n - 1, idx, val);
+  }
+
+  void update(int node, int start, int end, int idx, T val) {
+    if (start == end) {
+      tree[node] = val;
+    } else {
+      int mid = (start + end) / 2;
+      int leftChild = 2 * node + 1;
+      int rightChild = 2 * node + 2;
+
+      if (start <= idx && idx <= mid) {
+        update(leftChild, start, mid, idx, val);
+      } else {
+        update(rightChild, mid + 1, end, idx, val);
+      }
+
+      tree[node] = op(tree[leftChild], tree[rightChild]);
+    }
+  }
+};
+
+struct SumOp {
+  using T = ll;
+
+  T operator()(T a, T b) const {
+    return a + b;
+  }
+
+  T id() const {
+    return 0;
+  }
+};
+
+struct MinOp {
+  using T = ll;
+
+  T operator()(T a, T b) const {
+    return min(a, b);
+  }
+
+  T id() const {
+    return LLONG_MAX;
+  }
+};
+
+#define TESTCASES 1
+
+void solve() {
+  ll n;
+  cin >> n;
+  vll a(n), b(n);
+  SCAN(a);
+  SCAN(b);
+  ll m;
+  cin >> m;
+  map<ll, ll> d;
+  ll lastD;
+  REP (i, 0, m) {
+    ll x;
+    cin >> x;
+    lastD = x;
+
+    d[x]++;
+  }
+
+  REP (i, 0, n) {
+    if (a[i] != b[i]) {
+      if (d[b[i]] > 0) {
+        d[b[i]]--;
+        a[i] = b[i];
+      } else {
+        cout << "NO" << endl;
+        return;
+      }
+    }
+  }
+
+  bool found = false;
+  REP (i, 0, n) {
+    if (lastD == b[i]) {
+      found = true;
+      break;
+    }
+  }
+
+  cout << (found ? "YES" : "NO") << endl;
+
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int t = 1;
+  if (TESTCASES) cin >> t;
+  while (t--) solve();
+  return 0;
+}
